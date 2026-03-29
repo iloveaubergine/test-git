4bf3452dff6d7f17d6d3afb37eed3a0d58e7558e
iloveaubergine <iloveaubergine@gmail.com>
2026-03-29 03:17:36 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-
+document.querySelectorAll('a.btn').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
